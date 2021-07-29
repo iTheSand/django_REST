140759e2e0ccf2d95747829c5b81786a664afad5
iTheSand <thesand2009@yandex.ru>
2021-07-29 18:20:08 +0300

    lesson_5_hw

---
+++ frontend/src/components/UserProject.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const ProjectItem = ({project, users}) => {
+    return (
+        <tr>
+            <td>
+                {project.id}
+            </td>
+            <td>
+                {project.name}
+            </td>
+            <td>
+                {project.users.map((userId) => { return users.find((user) => user.id == userId).email } )}
+            </td>
+            <td>
+                {project.link_rep}
+            </td>
+        </tr>
+    )
+}
+
+const UserProjectList = ({project, users}) => {
+    let { id } = useParams();
+    let filtered_projects = project.filter((project) => project.users.includes(parseInt(id)));
+    return (
+        <table>
+            <th>
+                Id
+            </th>
+            <th>
+                Name
+            </th>
+            <th>
+                Users
+            </th>
+            <th>
+                LinkOfRep
+            </th>
+            {filtered_projects.map((project) => <ProjectItem project={project} users={users}/>)}
+        </table>
+    )
+}
+
+export default UserProjectList
---
+++ todo/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer, StringRelatedField
+
+from users.serializers import UserSerializer
+from .models import Project, Todo
+
+
+class ProjectSerializer(ModelSerializer):
+    # users = UserSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TodoSerializer(ModelSerializer):
+    project = StringRelatedField()
+    user = StringRelatedField()
+
+    class Meta:
+        model = Todo
+        fields = '__all__'
